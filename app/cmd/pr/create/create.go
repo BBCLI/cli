09f134f503e76f90ff9e70e31c9538fe13c30dab
Luis Carilla <luis.carilla@check24.de>
2024-05-24 23:26:42 +0200

    pr creation (currently with hardcoded repo)

--- app/cmd/pr/create/create.go
+++ app/cmd/pr/create/create.go
@@ -1,15 +1,67 @@
 package create
 
 import (
+	"bufio"
+	"context"
+	"errors"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
+
+	"cli/app/bbclient"
 )
 
 var Cmd = &cobra.Command{
 	Use:   "create",
-	Short: "Create a pull request",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Print("Prs index")
+	Short: "create a pull request",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 2 {
+			return errors.New("usage: pr create origin-branch destination-branch")
+		}
+		origin := args[0]
+		dest := args[1]
+
+		defaultTitle := origin + " to " + dest
+		fmt.Printf("Please enter PR title ('%s'): ", defaultTitle)
+		in := bufio.NewReader(os.Stdin)
+		title, err := in.ReadString('\n')
+		if err != nil {
+			return errors.New("error Reading your title")
+		}
+		title = strings.TrimSpace(title)
+		fmt.Println(title)
+		if title == "" {
+			title = defaultTitle
+		}
+
+		req := bbclient.PostRepositoriesWorkspaceRepoSlugPullrequestsJSONRequestBody{
+			Title: &title,
+			Destination: &bbclient.PullrequestEndpoint{
+				Branch: &struct {
+					DefaultMergeStrategy *string                                              `json:"default_merge_strategy,omitempty"`
+					MergeStrategies      *[]bbclient.PullrequestEndpointBranchMergeStrategies `json:"merge_strategies,omitempty"`
+					Name                 *string                                              `json:"name,omitempty"`
+				}{
+					Name: &dest,
+				},
+			},
+			Source: &bbclient.PullrequestEndpoint{
+				Branch: &struct {
+					DefaultMergeStrategy *string                                              `json:"default_merge_strategy,omitempty"`
+					MergeStrategies      *[]bbclient.PullrequestEndpointBranchMergeStrategies `json:"merge_strategies,omitempty"`
+					Name                 *string                                              `json:"name,omitempty"`
+				}{
+					Name: &origin,
+				},
+			},
+		}
+		res, err := bbclient.BbClient.PostRepositoriesWorkspaceRepoSlugPullrequestsWithResponse(context.TODO(), "check24", "tippspiel", req)
+		if err != nil || res.StatusCode() != 201 {
+			return errors.New("an error occurred")
+		}
+
+		return nil
 	},
 }
