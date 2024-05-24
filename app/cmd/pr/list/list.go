cec6458b44f4836910149f55fe1a37dd741112d1
Dominik Gic <dominik.gic@check24.de>
2024-05-24 21:47:02 +0200

    wip

--- app/cmd/pr/list/list.go
+++ app/cmd/pr/list/list.go
@@ -9,6 +9,7 @@ import (
 	"github.com/spf13/cobra"
 
 	"cli/app/bbclient"
+	config2 "cli/app/lib/config"
 )
 
 var Cmd = &cobra.Command{
@@ -19,9 +20,19 @@ var Cmd = &cobra.Command{
 		params := bbclient.GetPullrequestsSelectedUserParams{
 			State: (*bbclient.GetPullrequestsSelectedUserParamsState)(&state),
 		}
-		res, err := bbclient.BbClient.GetPullrequestsSelectedUserWithResponse(context.TODO(), "user-H-u20S8xQNiGvn-hnSLGqQ", &params)
+		config, err := config2.GetConfig()
 		if err != nil {
 			return err
+		}
+		if config.Authorization.Username == "" || config.Authorization.Password == "" {
+			return fmt.Errorf("please run 'bbc init' to initialize your Bitbucket Cloud CLI")
+		}
+		res, err := bbclient.BbClient.GetPullrequestsSelectedUserWithResponse(context.TODO(), config.Authorization.Username, &params)
+		if err != nil {
+			return err
+		}
+		if res.JSON200 == nil {
+			return fmt.Errorf("couldn't fetch PRs")
 		}
 		prs := *res.JSON200.Values
 
