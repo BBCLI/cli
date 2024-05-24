1249064fbbab75fc304fe1e1d09f29639718a6ea
Luis Carilla <luis.carilla@check24.de>
2024-05-24 21:34:40 +0200

    error handling

--- app/cmd/pr/list/list.go
+++ app/cmd/pr/list/list.go
@@ -3,7 +3,6 @@ package list
 import (
 	"context"
 	"fmt"
-	"log"
 	"os"
 	"text/tabwriter"
 
@@ -15,24 +14,30 @@ import (
 var Cmd = &cobra.Command{
 	Use:   "list",
 	Short: "list pull requests",
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
 		state := "OPEN"
 		params := bbclient.GetPullrequestsSelectedUserParams{
 			State: (*bbclient.GetPullrequestsSelectedUserParamsState)(&state),
 		}
 		res, err := bbclient.BbClient.GetPullrequestsSelectedUserWithResponse(context.TODO(), "user-H-u20S8xQNiGvn-hnSLGqQ", &params)
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
 		prs := *res.JSON200.Values
 
 		w := tabwriter.NewWriter(os.Stdout, 1, 1, 1, ' ', tabwriter.Debug)
 		for i := 0; i < len(prs); i++ {
 			pr := (prs)[i]
-			fmt.Fprintln(w, fmt.Sprintf("%v\t  %s", *pr.Id, *pr.Title))
+			_, err := fmt.Fprintln(w, fmt.Sprintf("%v\t  %s", *pr.Id, *pr.Title))
+			if err != nil {
+				return err
+			}
 		}
-		w.Flush()
-		//marshal, err := json.Marshal(res.JSON200)
-		//log.Printf("here: %s\n", marshal)
+		err = w.Flush()
+		if err != nil {
+			return err
+		}
+
+		return nil
 	},
 }
