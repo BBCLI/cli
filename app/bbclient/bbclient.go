f291ce28aca990eff9fda2a45a3870b1f8043f4f
Luis Carilla <luis.carilla@check24.de>
2024-05-24 21:29:28 +0200

    WIP

--- app/bbclient/bbclient.go
+++ app/bbclient/bbclient.go
@@ -1,2 +1,24 @@
 //go:generate go run github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen --config=config.yaml bbclient.json
 package bbclient
+
+import (
+	"context"
+	b64 "encoding/base64"
+	"log"
+	"net/http"
+)
+
+var BbClient *ClientWithResponses
+
+func init() {
+	var err error
+	hc := http.Client{}
+	BbClient, err = NewClientWithResponses("https://api.bitbucket.org/2.0", WithHTTPClient(&hc), WithRequestEditorFn(func(ctx context.Context, req *http.Request) error {
+		req.Header.Add("Authorization", "Basic "+b64.StdEncoding.EncodeToString([]byte("user:password")))
+		return nil
+	}))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
--- app/cmd/pr/list/list.go
+++ app/cmd/pr/list/list.go
@@ -1,15 +1,38 @@
 package list
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"os"
+	"text/tabwriter"
 
 	"github.com/spf13/cobra"
+
+	"cli/app/bbclient"
 )
 
 var Cmd = &cobra.Command{
 	Use:   "list",
 	Short: "list pull requests",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Print("List of Prs")
+		state := "OPEN"
+		params := bbclient.GetPullrequestsSelectedUserParams{
+			State: (*bbclient.GetPullrequestsSelectedUserParamsState)(&state),
+		}
+		res, err := bbclient.BbClient.GetPullrequestsSelectedUserWithResponse(context.TODO(), "user-H-u20S8xQNiGvn-hnSLGqQ", &params)
+		if err != nil {
+			log.Fatal(err)
+		}
+		prs := *res.JSON200.Values
+
+		w := tabwriter.NewWriter(os.Stdout, 1, 1, 1, ' ', tabwriter.Debug)
+		for i := 0; i < len(prs); i++ {
+			pr := (prs)[i]
+			fmt.Fprintln(w, fmt.Sprintf("%v\t  %s", *pr.Id, *pr.Title))
+		}
+		w.Flush()
+		//marshal, err := json.Marshal(res.JSON200)
+		//log.Printf("here: %s\n", marshal)
 	},
 }
