dbfe08737957799a1a6108f8d7bf15e73721b8d3
Luis Carilla <luis.carilla@check24.de>
2024-05-25 10:06:27 +0200

    fix

--- app/cmd/pr/approve/approve.go
+++ app/cmd/pr/approve/approve.go
@@ -3,6 +3,7 @@ package approve
 import (
 	"context"
 	"errors"
+	"fmt"
 	"strconv"
 
 	"github.com/spf13/cobra"
@@ -17,7 +18,7 @@ var Cmd = &cobra.Command{
 	Long:  "usage: bbcli pr approve <pullrequest-id>",
 	Args:  cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		prId, err := strconv.Atoi(args[1])
+		prId, err := strconv.Atoi(args[0])
 		if err != nil {
 			return err
 		}
@@ -31,6 +32,8 @@ var Cmd = &cobra.Command{
 		if err != nil || res.StatusCode() != 200 {
 			return errors.New("an error occurred")
 		}
+		fmt.Println("pr approved successfully")
+
 		return nil
 	},
 }
